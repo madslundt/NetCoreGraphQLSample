b448e9aed2907839dad00138378940d86d43a39b
Mads Engel Lundt <madslundt@live.dk>
2019-04-11 13:21:47 +0200

    Using nameof user model in description

--- Features/User/Types/UserType.cs
+++ Features/User/Types/UserType.cs
@@ -6,10 +6,10 @@ namespace Features.User.Types
     {
         public UserType()
         {
-            Field(x => x.Id, type: typeof(NonNullGraphType<IdGraphType>)).Description($"ID of {Name}");
-            Field(x => x.FirstName, type: typeof(NonNullGraphType<StringGraphType>)).Description($"First name of {Name}");
-            Field(x => x.LastName, type: typeof(NonNullGraphType<StringGraphType>)).Description($"Last name of {Name}");
-            Field(x => x.Status, type: typeof(NonNullGraphType<UserStatusType>)).Description($"Status of {Name}");
+            Field(x => x.Id, type: typeof(NonNullGraphType<IdGraphType>)).Description($"ID of {nameof(DataModel.Models.User)}");
+            Field(x => x.FirstName, type: typeof(NonNullGraphType<StringGraphType>)).Description($"First name of {nameof(DataModel.Models.User)}");
+            Field(x => x.LastName, type: typeof(NonNullGraphType<StringGraphType>)).Description($"Last name of {nameof(DataModel.Models.User)}");
+            Field(x => x.Status, type: typeof(NonNullGraphType<UserStatusType>)).Description($"Status of {nameof(DataModel.Models.User)}");
         }
     }
 }
